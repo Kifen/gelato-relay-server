9fbaba3b205c4bd0e5dfee165a29a6a0ffc93d0a
kifen <kifenraph@gmail.com>
2021-07-02 16:35:47 -0700

    set up project relay-server

---
+++ relay-server/config.ts
@@ -0,0 +1,5 @@
+export default {
+  RELAYER_SERVER_URL: "http://localhost:3000/api/v1/relay",
+  PORT: 3000,
+  RELAYER_ADDRESS: ""
+}
---
+++ relay-server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import config from '../config';
+//import { route } from './relayer/routes';
+
+const app: Express = express();
+const PORT: number = config.PORT || 5000;
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, OPTIONS");
+//   res.header("Access-Control-Allow-Methods", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next()
+// })
+app.use(bodyParser.json());
+app.use(express.json({limit: "50mb"}))
+//app.use("/", route);
+
+app.listen(PORT, function () {
+  console.log(`App is listening on port ${PORT} !`)
+});
