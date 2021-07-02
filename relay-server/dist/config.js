6401ec6fa021d729defb71a9b7f0567a2ed7a379
kifen <kifenraph@gmail.com>
2021-07-02 16:37:58 -0700

    add .gitignore

--- relay-server/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    RELAYER_SERVER_URL: "http://localhost:3000/api/v1/relay",
-    PORT: 3000,
-    RELAYER_ADDRESS: ""
-};
