ec44280337e360e3da5b7b578a12ce3b1c657fc9
kifen <kifenraph@gmail.com>
2021-07-03 11:34:48 -0700

    deploy dai mock to ropsten and generate signature

--- relay-server/src/limit-order-lib/order.ts
+++ relay-server/src/limit-order-lib/order.ts
@@ -1,6 +1,32 @@
 import { Signer } from '@ethersproject/abstract-signer';
-import { Approve } from './types';
+import { Provider } from "@ethersproject/abstract-provider";
+import { ChainId, Handler } from "@gelatonetwork/limit-orders-lib";
+import { Approve, MAX_INT } from './types';
 import { utils, BigNumberish, Signature } from 'ethers';
 import config from '../../config';
+import { providerIsValid, signerIsValid, generateSignature, daiContract } from './utils';
 
-export const submitLimitOrder = async (signerOrProvider: Signer) => {}
+export const submitLimitOrder = async (
+  signerOrProvider: Signer,
+  inputToken: string,
+  outputToken: string,
+  inputAmount: BigNumberish,
+  minReturn: BigNumberish,
+  handler: Handler,
+  chainId: ChainId,  
+  expiry: number,
+) => {
+  if (!(chainId as ChainId)) throw new Error("NETWORK NOT SUPPORTED");
+  if (!signerIsValid(signerOrProvider)) throw new Error("Invalid Signer");
+  if (!providerIsValid(signerOrProvider)) throw new Error("Invalid Provider");
+
+  const owner = await signerOrProvider?.getAddress();
+  const contract = await daiContract(signerOrProvider);
+  const nonce = await contract.nonces(owner);
+  const approve = {
+    spender: config.RELAYER_PROXY_ADDRESS,
+    holder: owner,
+    allowed: true
+  }
+  const signature = await generateSignature(signerOrProvider, chainId, nonce, expiry, approve);
+}
--- relay-server/src/limit-order-lib/utils.ts
+++ relay-server/src/limit-order-lib/utils.ts
@@ -1,7 +1,9 @@
-import { utils, BigNumberish, Signature } from 'ethers';
+import { utils, BigNumberish, Signature, Contract } from 'ethers';
 import { Signer } from '@ethersproject/abstract-signer';
-import { Approve } from './types';
+import { Provider } from "@ethersproject/abstract-provider";
+import { Approve, DAI_ABI } from './types';
 import config from '../../config';
+import ABI from "../../abi/dai.json";
 
 // The PERMIT_TYPEHASH was gotten from https://github.com/makerdao/dss/blob/3f30552a586264b32ebdb5bac94ba67020282e53/src/dai.sol#L59
 export const PERMIT_TYPEHASH = '0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb';
@@ -42,4 +44,25 @@ export const generateSignature = async (signerOrProvider: Signer, chainId: numbe
   const digest = generateDaiDigest(config.DAI_VERSION, config.DAI_NAME, chainId, nonce, expiry, approve);
   const data = await signerOrProvider.signMessage(digest)
   return utils.splitSignature(data); 
+}
+
+export const signerIsValid = (signer: Signer): boolean => {
+  if (Signer.isSigner(signer)) return true;
+  return false;
+}
+
+export const providerIsValid = (signerOrProvider: Signer): boolean => {
+  const provider = Provider.isProvider(signerOrProvider) 
+    ? signerOrProvider
+    : Signer.isSigner(signerOrProvider)
+    ? signerOrProvider.provider
+    : undefined;
+
+  if (provider === undefined) return false;
+
+  return true;
+}
+
+export const daiContract = async (provider: Signer): Promise<Contract> => {
+  return new Contract(config.DAI_ADDRESS, ABI.abi, provider);
 }
