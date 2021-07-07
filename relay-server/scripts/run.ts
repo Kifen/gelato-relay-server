f89c3f45481eb521af0434409c48ee4b235c12e2
kifen <kifenraph@gmail.com>
2021-07-07 11:33:13 +0100

    write CLI to script

--- relay-server/scripts/run.ts
+++ relay-server/scripts/run.ts
@@ -1,16 +1,36 @@
 import {submitLimitOrder} from '../src/relay-order-lib';
 import { Wallet, providers, BigNumber } from 'ethers';
 import { PK, DAI_ADDRESS, RPC_URL } from "../src/relay-order-lib/constants";
-console.log(PK, DAI_ADDRESS, RPC_URL)
+import { program } from "commander";
+
 const inputToken = DAI_ADDRESS; // DAI
 const outputToken = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"; // ETH
-const inputAmount: BigNumber = BigNumber.from("11000000000000000000");
-const minReturn: BigNumber = BigNumber.from("3000000000000000000");
+
+
+program
+  .requiredOption('-a, --amount <type>', 'amount to swap')
+  .requiredOption('-m, --minreturn <type>', 'minimum amount expected')
+
+program.parse(process.argv);
+const options = program.opts();
+
+let inputAmount: BigNumber = BigNumber.from(`${options.amount}000000000000000000`);
+let minReturn: BigNumber = BigNumber.from(`${options.minreturn}000000000000000000`);
 
 const run = (async () => {
+  if(!check()) {
+    console.log("Minimum return amount cannot be greater than the input amount")
+    return;
+  }
+
   const provider = new providers.JsonRpcProvider(RPC_URL)
   const pk = PK
   const signer = new Wallet(pk);
   const signerOrProvider  = await signer.connect(provider);
   await submitLimitOrder(signerOrProvider, inputToken, outputToken, inputAmount, minReturn, "uniswap", 3, Date.now() + 120, pk);
 })()
+
+function check(): boolean {
+  if (parseInt(options.amount) < parseInt(options.minreturn)) return false;
+  return true;
+}
